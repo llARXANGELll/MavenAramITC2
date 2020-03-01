c5a2e5b41ef8cf1514f7d8662c21a17a7bdbc2af
llARXANGELll <aramh@inbox.ru>
2020-03-01 22:32:52 +0300

    Настроил лог

---
+++ src/main/java/Work12/Task1/Main.java
@@ -0,0 +1,17 @@
+package Work12.Task1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Work12.Task1.Main.class);
+    public static void main(String[] args) {
+        LOGGER.info("info");
+        LOGGER.error("error");
+        LOGGER.warn("warn");
+        LOGGER.trace("trace");
+
+    }
+
+}
+
