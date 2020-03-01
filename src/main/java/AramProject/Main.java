d45b2edc59437002ddd7150d818b0436e9d4bacb
llARXANGELll <aramh@inbox.ru>
2020-03-01 14:34:17 +0300

    test4

--- src/main/java/AramProject/Main.java
+++ src/main/java/AramProject/Main.java
@@ -7,7 +7,9 @@ import org.slf4j.LoggerFactory;
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
+        System.out.println("dsd");
         LOGGER.info("Hi");
+
     }
 
 }
