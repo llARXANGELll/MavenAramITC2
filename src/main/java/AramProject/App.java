3f6e8e61c195eea7b0f5ebca88e58df8e48791f6
llARXANGELll <aramh@inbox.ru>
2020-03-01 03:08:48 +0300

    test3

---
+++ src/main/java/AramProject/App.java
@@ -0,0 +1,9 @@
+package AramProject;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/AramProject/Main.java
@@ -0,0 +1,10 @@
+package AramProject;
+import org.slf4j.Logger;
+
+public class Main {
+    private static final Logger
+    public static void main(String[] args) {
+
+    }
+
+}
