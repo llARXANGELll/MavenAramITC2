629c5b56f2f9ce2c3482949b64a8a9106d537446
llARXANGELll <aramh@inbox.ru>
2020-03-01 03:50:56 +0300

    test3

--- src/main/java/AramProject/Main.java
+++ src/main/java/AramProject/Main.java
@@ -1,10 +1,13 @@
 package AramProject;
+
+
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
-    private static final Logger
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        
+        LOGGER.info("Hi");
     }
 
 }
